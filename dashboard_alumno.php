248c9c2de9decca33f18f877dff0859b021f8dc6
S3RN4K <149739380+S3RN4K@users.noreply.github.com>
2024-02-24 10:23:33 -0500

    Update dashboard_alumno.php

--- dashboard_alumno.php
+++ dashboard_alumno.php
@@ -340,7 +340,7 @@ try {
             </div>
             <ul>
                 <li><a href="dashboard_alumno.php">Inicio</a></li>
-                <li><a href="notas.php">Notas</a></li>
+                <li><a href="cursos.php">Cursos</a></li>
                 <li><a href="dashboard_tutorias.php">Tutorías</a></li>
                 <li><a href="horario.php">Horario</a></li>
                 <li><a href="objetivos.php">Objetivos</a></li>
